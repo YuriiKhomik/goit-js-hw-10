c623bd1a07928fc132be70716e2b960de091cab2
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-31 08:37:25 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const refs = {
 refs.input.addEventListener('input', debounce(onInputType, DEBOUNCE_DELAY));
 
 function onInputType(e) {
-    inputQuery = refs.input.value;
+    inputQuerry = refs.input.value.trim();
     console.log(inputQuery)
     
     clearHtml();
