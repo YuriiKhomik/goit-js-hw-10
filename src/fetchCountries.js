c3a7c703b439111848f5b6070e8922df1932942e
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-10-31 07:50:14 +0200

    ...

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name'
+
+export function fetchCountries(name) {
+    
+    const url = `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`;
+
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    return [];
+                };
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+};
