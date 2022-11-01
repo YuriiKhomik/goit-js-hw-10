06c70336c8939a3b8dd4cf6a6a4b24c743a8cff4
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-11-01 12:59:05 +0200

    ...

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,18 +1,32 @@
-const BASE_URL = 'https://restcountries.com/v3.1/name'
+// const BASE_URL = 'https://restcountries.com/v3.1/name'
 
-export class RestCountriesService {
-    fetchCountries(name) {
-        const url = `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`;
+// export class RestCountriesService {
+//     fetchCountries(name) {
+//         const url = `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`;
 
-        return fetch(url)
-            .then(response => {
-                if (response.status === 404) {
-                    return [];
-                }
-                return response.json();
-            })
-            .catch(error => {
-            throw new Error(response.status)
-        })
-        }
-};
+//         return fetch(url)
+//             .then(response => {
+//                 if (response.status === 404) {
+//                     return [];
+//                 }
+//                 return response.json();
+//             })
+//             .catch(error => {
+//             throw new Error(response.status)
+//         })
+//         }
+// };
+
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+
+export function fetchCountries(name) {
+  const url = `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`;
+
+  return fetch(url).then(response => {
+    if (response.status === 404) {
+      console.error(response.status);
+      return [];
+    }
+    return response.json();
+  });
+}
