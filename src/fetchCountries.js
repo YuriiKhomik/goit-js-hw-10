047615560bbf9bd05036c104beb9a0b9021f2994
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-11-01 15:52:03 +0200

    ...

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -17,6 +17,20 @@
 //         }
 // };
 
+// const BASE_URL = 'https://restcountries.com/v3.1/name';
+
+// export function fetchCountries(name) {
+//   const url = `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`;
+
+//   return fetch(url).then(response => {
+//     if (response.status === 404) {
+//       console.error(response.status);
+//       return [];
+//     }
+//     return response.json();
+//   });
+// };
+
 const BASE_URL = 'https://restcountries.com/v3.1/name';
 
 export function fetchCountries(name) {
@@ -24,8 +38,7 @@ export function fetchCountries(name) {
 
   return fetch(url).then(response => {
     if (response.status === 404) {
-      console.error(response.status);
-      return [];
+      throw 'Oops, there is no country with that name';
     }
     return response.json();
   });
